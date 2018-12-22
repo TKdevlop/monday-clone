12936e37847ef4bbed58d16b3a3171711af84e15
tushar <tusharkharbanda13@gmail.com>
2018-12-22 21:53:45 +0530

    intital commit

---
+++ precache-manifest.97bc2a02ef67dd7d45229917d6c4a4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "952ab156ccdd77f0fb97",
+    "url": "/static/js/main.952ab156.chunk.js"
+  },
+  {
+    "revision": "eb3998f22056fe302a46",
+    "url": "/static/js/1.eb3998f2.chunk.js"
+  },
+  {
+    "revision": "952ab156ccdd77f0fb97",
+    "url": "/static/css/main.439ccd52.chunk.css"
+  },
+  {
+    "revision": "7f9525c711d9f399a4df232574d9427f",
+    "url": "/index.html"
+  }
+];
