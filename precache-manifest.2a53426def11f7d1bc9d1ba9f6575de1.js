2ffe8e185b805fb189f5266f0d269012c0f41b55
tushar <tusharkharbanda13@gmail.com>
2019-01-02 10:27:26 +0530

    updated

---
+++ precache-manifest.2a53426def11f7d1bc9d1ba9f6575de1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b283ce48e2e70b3dad60",
+    "url": "/static/js/main.b283ce48.chunk.js"
+  },
+  {
+    "revision": "3b943d912a05da05cbd0",
+    "url": "/static/js/1.3b943d91.chunk.js"
+  },
+  {
+    "revision": "b283ce48e2e70b3dad60",
+    "url": "/static/css/main.24a0ede1.chunk.css"
+  },
+  {
+    "revision": "d479dc3c7828eb6b4f01a238dfa53766",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8daddbc958bf06d774eadc4b50739034.js"
+  "/precache-manifest.2a53426def11f7d1bc9d1ba9f6575de1.js"
 );
 
 workbox.clientsClaim();
